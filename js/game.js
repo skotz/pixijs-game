2bcc14f0f547dae67f180caf7aa286d9efa64ff0
Scott Clayton <scottclayton84@gmail.com>
2019-04-20 11:59:38 -0500

    Added ships

--- js/game.js
+++ js/game.js
@@ -20,40 +20,74 @@ function resize() {
 }
 resize();
 
-// Initialize actors
-var actors = [];
-actors.push({
+// Settings
+const growthFactor = 2.0;
+
+// Initialize planets
+var planets = [];
+planets.push({
     team: 1,
     color: 0x2fe02a,
-    x: 200,
+    x: 300,
     y: 250,
     power: 100,
     level: 2,
-    sprite: {}
+    sprite: {},
+    ships: []
 },{
     team: 2,
     color: 0x2a88e0,
-    x: 600,
+    x: 700,
     y: 450,
     power: 50,
     level: 1,
-    sprite: {}
+    sprite: {},
+    ships: []
+},{
+    team: 2,
+    color: 0xe02a63,
+    x: 500,
+    y: 650,
+    power: 10,
+    level: 5,
+    sprite: {},
+    ships: []
 });
 
-// Methods
+// Get the size of a circle based on it's power level
 var getSizeFromPower = function (power) {
     // The power is the area of a circle, so the size is the diameter
-    return Math.sqrt(power / Math.PI) * 5;
+    return Math.sqrt(power / Math.PI) * 10;
+}
+
+// Create a ship for a given player
+var createShip = function (actor, dx, dy) {
+    const graphics = new PIXI.Graphics();
+    const size = 2;
+    graphics.beginFill(actor.color);
+    graphics.lineStyle(0);
+    graphics.drawCircle(size, size, size);
+    graphics.endFill();
+
+    const texture = PIXI.RenderTexture.create(graphics.width, graphics.height);
+    app.renderer.render(graphics, texture);
+
+    const sprite = new PIXI.Sprite(texture);
+    sprite.x = actor.x + dx;
+    sprite.y = actor.y + dy;
+    sprite.anchor.set(0.5, 0.5);
+    
+    return sprite;
 }
 
 // Initialize objects
-for (var i = 0; i < actors.length; i++) {
+for (var i = 0; i < planets.length; i++) {
     
-    const diameter = getSizeFromPower(actors[i].power);
+    const diameter = getSizeFromPower(planets[i].power);
     
     const graphics = new PIXI.Graphics();
     const upscale = 1000;
-    graphics.beginFill(actors[i].color);
+    graphics.beginFill(planets[i].color);
     graphics.lineStyle(0);
     graphics.drawCircle(upscale, upscale, upscale);
     graphics.endFill();
@@ -61,22 +95,30 @@ for (var i = 0; i < actors.length; i++) {
     const texture = PIXI.RenderTexture.create(graphics.width, graphics.height);
     app.renderer.render(graphics, texture);
 
-    actors[i].sprite = new PIXI.Sprite(texture);
-    actors[i].sprite.x = actors[i].x;
-    actors[i].sprite.y = actors[i].y;
-    actors[i].sprite.anchor.set(0.5, 0.5);
-    actors[i].sprite.interactive = true;
-    actors[i].sprite.hitArea = new PIXI.Circle(0, 0, upscale);
+    planets[i].sprite = new PIXI.Sprite(texture);
+    planets[i].sprite.x = planets[i].x;
+    planets[i].sprite.y = planets[i].y;
+    planets[i].sprite.anchor.set(0.5, 0.5);
+    planets[i].sprite.interactive = true;
+    planets[i].sprite.hitArea = new PIXI.Circle(0, 0, upscale);
     
-    const sprite = actors[i].sprite;
-    actors[i].sprite.pointerover = function() {
+    const sprite = planets[i].sprite;
+    planets[i].sprite.pointerover = function() {
         sprite.alpha = .5;
     };
-    actors[i].sprite.pointerout = function() {
+    planets[i].sprite.pointerout = function() {
         sprite.alpha = 1;
     };
     
-    app.stage.addChild(actors[i].sprite);
+    // for (var s = 0; s < planets[i].power; s++) {
+        // var dx = Math.random() * 100 - 50;
+        // var dy = Math.random() * 100 - 50;
+        // const ship = createShip(planets[i], dx, dy);
+        // planets[i].ships.push(ship);
+        // app.stage.addChild(ship);
+    // }
+    
+    app.stage.addChild(planets[i].sprite);
 }
 
 // Game loop
@@ -84,12 +126,46 @@ app.ticker.add((delta) => {
     // Number of seconds since the last frame
     var seconds = app.ticker.elapsedMS / 1000;
     
-    // Update strengths
-    for (var i = 0; i < actors.length; i++) {
-        actors[i].power += actors[i].level * seconds;
-        var size = getSizeFromPower(actors[i].power);
-        actors[i].sprite.width = size;
-        actors[i].sprite.height = size;
+    for (var i = 0; i < planets.length; i++) {        
+        // Update strengths
+        planets[i].power += planets[i].level * seconds * growthFactor;
+        var size = getSizeFromPower(planets[i].power);
+        planets[i].sprite.width = size;
+        planets[i].sprite.height = size;
+        
+        // Add more ships
+        while (planets[i].ships.length < planets[i].power) {
+            var dx = Math.random() * 100 - 50;
+            var dy = Math.random() * 100 - 50;
+            const ship = createShip(planets[i], dx, dy);
+            planets[i].ships.push(ship);
+            app.stage.addChild(ship);
+        }
+        
+        // Update the ships
+        for (var s = 0; s < planets[i].ships.length; s++) {            
+            // Random movement
+            var range = 4;
+            var dx = Math.random() * range - range / 2;
+            var dy = Math.random() * range - range / 2;
+            
+            // Keep them somewhat close to the planet
+            var distx = planets[i].sprite.x - planets[i].ships[s].x;
+            var disty = planets[i].sprite.y - planets[i].ships[s].y;
+            var dist = Math.sqrt(distx * distx + disty * disty);
+            if (dist > size) {
+                dx += distx * 0.001;
+                dy += disty * 0.001;
+            }
+            if (dist < size) {
+                dx -= distx * 0.001;
+                dy -= disty * 0.001;
+            }
+            
+            // Update ship location
+            planets[i].ships[s].x += dx;
+            planets[i].ships[s].y += dy;
+        }
     }
 });
 
